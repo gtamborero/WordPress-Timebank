99662dbcb651eec0382cd49d102800b0887715ca
guillermo <gtamborero@iproject.cat>
2023-01-27 23:46:09 +0100

    Add amount as field + list and search

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,6 @@
 <?php
 
+// ADD CUSTOM TIMEBANK BOX INSIDE ADD / EDIT TRANSACTION
 function tbank_custom_box() {
 		add_meta_box(
 			'tbank_box_id',                 // Unique ID
@@ -12,50 +13,59 @@ function tbank_custom_box() {
 }
 add_action( 'add_meta_boxes', 'tbank_custom_box' );
 
+
+// ADD CUSTOM HTML BOX INSIDE ADD / EDIT TRANSACTION
 function tbank_custom_box_html( $post ) {
 	$timebank_payer = get_post_meta( $post->ID, '_timebank_payer', true );
-    $timebank_giver = get_post_meta( $post->ID, '_timebank_giver', true );
-	?>
-    <div style="display:grid; grid-template-columns:1fr 1fr; gap:20px;">
-
-    <label >Time Payer:</label>
-    <label>Time Receiver:</label>
-
-	<select name="timebank_payer" id="timebank_payer" class="postbox">
-
-		<option value="">Select user...</option>
-        <?php
-        $users = get_users();
-        foreach ($users as $user){ 
-        ?>
-
-            <option value="<?php echo $user->user_login; ?>" <?php selected( $timebank_payer, $user->user_login ); ?>>
-                <?php echo $user->user_login; ?>
-            </option>
-
-        <?php } ?>
-	</select>
-
+    $timebank_receiver = get_post_meta( $post->ID, '_timebank_receiver', true );
+    $timebank_amount = get_post_meta( $post->ID, '_timebank_amount', true );
     
-	<select name="timebank_giver" id="timebank_giver" class="postbox">
+?>
+<div style="display:grid; grid-template-columns:1fr 1fr; gap:10px;">
+
+    <label> Time Payer:<br>
+        <select name="timebank_payer" id="timebank_payer" class="postbox">
+
+            <option value="">Select user...</option>
+            <?php
+            $users = get_users();
+            foreach ($users as $user){ 
+            ?>
+
+                <option value="<?php echo $user->user_login; ?>" <?php selected( $timebank_payer, $user->user_login ); ?>>
+                    <?php echo $user->user_login; ?>
+                </option>
+
+            <?php } ?>
+        </select>
+    </label>
+
+    <label>Time Receiver:<br>
+	    <select name="timebank_receiver" id="timebank_receiver" class="postbox">
     
-		<option value="">Select user...</option>
-        <?php
-        $users = get_users();
-        foreach ($users as $user){ 
-        ?>
+            <option value="">Select user...</option>
+            <?php
+            $users = get_users();
+            foreach ($users as $user){ 
+            ?>
 
-            <option value="<?php echo $user->user_login; ?>" <?php selected( $timebank_giver, $user->user_login ); ?>>
-                <?php echo $user->user_login; ?>
-            </option>
+                <option value="<?php echo $user->user_login; ?>" <?php selected( $timebank_receiver, $user->user_login ); ?>>
+                    <?php echo $user->user_login; ?>
+                </option>
 
-        <?php } ?>
-	</select>
+            <?php } ?>
+        </select>
+    </label>
 
-        </div>
-	<?php
+    <label> Amount:<br>
+        <input type="text" id="timebank_amount" name="timebank_amount" value="<?php echo $timebank_amount; ?>">
+    </label>
+
+</div>
+<?php
 }
 
+// SAVE TIMEBANK CUSTO DATA ON PUBLISH
 function tbank_save_postdata( $post_id ) {
 	if ( array_key_exists( 'timebank_payer', $_POST ) ) {
 		update_post_meta(
@@ -64,18 +74,25 @@ function tbank_save_postdata( $post_id ) {
 			$_POST['timebank_payer']
 		);
 	}
-    if ( array_key_exists( 'timebank_giver', $_POST ) ) {
+    if ( array_key_exists( 'timebank_receiver', $_POST ) ) {
 		update_post_meta(
 			$post_id,
-			'_timebank_giver',
-			$_POST['timebank_giver']
+			'_timebank_receiver',
+			$_POST['timebank_receiver']
+		);
+	}
+    if ( array_key_exists( 'timebank_amount', $_POST ) ) {
+		update_post_meta(
+			$post_id,
+			'_timebank_amount',
+			$_POST['timebank_amount']
 		);
 	}
 }
 add_action( 'save_post', 'tbank_save_postdata' );
 
 
-// ADD COLUMN ON TRANSACTIONS POST TYPE
+// ADD COLUMN ON ADMIN LIST VIEW OF TRANSACTIONS POST TYPE
 function tbank_add_admin_column( $column_title, $post_type, $cb, $order_by = false, $order_by_field_is_meta = false ){
 
     // Column Header
@@ -119,15 +136,21 @@ function tbank_add_admin_column( $column_title, $post_type, $cb, $order_by = fal
 
 }
 
+
+// CUSTOM ADMIN COLUMNS
 tbank_add_admin_column(__('Time Payer'), 'tbank-transaction', function($post_id){
-    //var_dump(get_post_meta( $post_id ));
+//var_dump(get_post_meta( $post_id ));
     echo get_post_meta( $post_id , '_timebank_payer' , true ); 
 }, '_timebank_payer', true);
 
 tbank_add_admin_column(__('Time Giver'), 'tbank-transaction', function($post_id){
-    //var_dump(get_post_meta( $post_id ));
-    echo get_post_meta( $post_id , '_timebank_giver' , true ); 
-}, '_timebank_giver', true);
+    echo get_post_meta( $post_id , '_timebank_receiver' , true ); 
+}, '_timebank_receiver', true);
+
+tbank_add_admin_column(__('Amount'), 'tbank-transaction', function($post_id){
+    echo get_post_meta( $post_id , '_timebank_amount' , true ); 
+}, '_timebank_amount', true);
+
 
 // CUSTOM SEARCH INSIDE META (para buscar usuarios timebank)
 // busca bien por titulo y payer y seller
