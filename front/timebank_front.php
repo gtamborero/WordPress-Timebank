2ae3bc30549ea8b8a540302a4997f4caefb3fae4
guillermo <gtamborero@iproject.cat>
2023-02-26 19:18:05 +0100

    front with user data exchanges

--- front/timebank_front.php
+++ front/timebank_front.php
@@ -12,16 +12,55 @@ $userId = get_current_user_id();
 $args = array(
     'post_type' => 'tbank-transaction',
     'meta_query' => array(
+       
+        'relation' => 'OR',
         array(
             'key' => '_timebank_payer',
             'value' => $userId,
-            'compare' => 'LIKE',
+            'compare' => '=',
         ),
+        array(
+            'key' => '_timebank_receiver',
+            'value' => $userId,
+            'compare' => '=',
+        ),
+        
     ),
 );
 $query = new WP_Query( $args );
+?>
 
+<div style="display:grid; grid-template-columns:1fr 1fr 1fr 1fr 1fr 1fr 1fr; background-color:#333; color:#fff; padding:10px;">
+    <div>Fecha</div>
+    <div>Descripción</div>
+    <div>Receptor</div>
+    <div>Importe</div>
+    <div>Saldo</div>
+    <div>Valoración</div>
+    <div>Comentario</div>
+</div>
+
+<div style="display:grid; grid-template-columns:1fr 1fr 1fr 1fr 1fr 1fr 1fr; padding:10px;">
+<?php
+    if ( $query->have_posts() ){
+        while ( $query->have_posts() ){
+             $query->the_post(); 
+            ?>
+            <div>1 Feb</div>
+            <div><?php the_title(); ?></div>
+            <div>Receptor</div>
+            <div>importe</div>
+            <div>Saldo</div>
+            <div>Valoración</div>
+            <div>Comentario</div>
+<?php
+        }
+    } 
+?>
+</div>
+
+<?php
 echo "<pre>"; 
-var_dump($query);
+var_dump($query->posts);
 ?> 
 
