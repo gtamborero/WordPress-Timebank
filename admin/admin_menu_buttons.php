4231afefb9aa2d5eedb08a7d30a36abbdb31da24
guillermo <gtamborero@iproject.cat>
2023-03-30 22:42:25 +0200

    Update admin_menu_buttons.php

--- admin/admin_menu_buttons.php
+++ admin/admin_menu_buttons.php
@@ -4,5 +4,12 @@
 add_action( 'admin_menu', 'timebank_menu' );
 function timebank_menu() {
 	//add_menu_page( __('TimeBank' , 'timebank'), 'TimeBank', 'manage_options', 'timebank', 'timebank_exchanges' );
-	add_submenu_page( 'edit.php?post_type=tbank-transaction', __('Configuration' , 'timebank'), 'Configuration', 'manage_options', 'timebank_options', 'timebank_options');
+	add_submenu_page( 
+		'edit.php?post_type=tbank-transaction', 
+		__('Configuration' , 'timebank'), 
+		'Configuration', 
+		'manage_options', 
+		'timebank_options', 
+		'timebank_options'
+	);
 }
